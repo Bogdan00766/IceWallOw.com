68daa52e528473645d9cb408aed4d5c3c59841c6
Piotr "Bogdan" Chudzik-Lipka <p.chudziklipka0@gmail.com>
2022-05-12 14:49:08 +0200

    Create ASP.NET Api

---
+++ DbInfrastructure/Migrations/20220512124249_1.2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbInfrastructure.Migrations
+{
+    public partial class _12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Category",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Category");
+        }
+    }
+}
---
+++ IceWallOw.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using DbInfrastructure;
+using Domain;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IceWallOw.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet(Name = "GetProducts")]
+        public async Task<List<Product>> Get()
+        {
+            using(IUnitOfWork uow = new UnitOfWork())
+            {
+                return await uow.ProductRepository.FindAllAsync();
+            }
+            
+        }
+    }
+}
