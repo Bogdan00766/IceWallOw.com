60d0718dffcae7236b98845ccb27743faf62003c
Szymon Kordal <szymon.kordal@student.wat.edu.pl>
2022-05-14 18:24:43 +0200

    nesting components

--- icewallow.react/src/index.js
+++ icewallow.react/src/index.js
@@ -4,7 +4,7 @@ import "jquery"
 import "bootstrap/dist/js/bootstrap"
 import "popper.js/dist/umd/popper"
 import "bootstrap/dist/css/bootstrap.css"
-import { NavBar } from "./App.jsx"
+import App from "./App.jsx"
 import "./index.css"
 
-ReactDOM.render(<NavBar/>, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
