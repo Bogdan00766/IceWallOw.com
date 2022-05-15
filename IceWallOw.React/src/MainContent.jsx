8872ffd92952cd923d73a4f953d642ba82ec3f12
Szymon Kordal <szymon.kordal@student.wat.edu.pl>
2022-05-15 13:23:44 +0200

    getting event

--- IceWallOw.React/src/MainContent.jsx
+++ IceWallOw.React/src/MainContent.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 export default class MainContent extends Component {
 
-    state = { appTitle: "Customers" };
+    state = { pageTitle: "Customers", customersCount: 5 };
 
     render() {
         return (
             <div>
-                <h4 className="border-bottom m-1 p-1"> {this.state.appTitle}</h4>
+                <h4 className="border-bottom m-1 p-1"> {this.state.pageTitle}
+                    <span style={{ backgroundColor: "#6c757d" }} className="badge badge-secondary" >{this.state.customersCount}</span>
+                    <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
+                </h4>
             </div>
         )
+    }
+
+
+    onRefreshClick() {
+        console.log("refresh click")
     }
 }
