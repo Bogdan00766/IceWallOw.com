72ccdd2a23226da8bc8d95c6c3638de0f18e766c
Szymon Kordal <szymon.kordal@student.wat.edu.pl>
2022-05-15 13:10:46 +0200

    render xpressions

--- IceWallOw.React/src/MainContent.jsx
+++ IceWallOw.React/src/MainContent.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-export default class MainContent extends Component{
+export default class MainContent extends Component {
+
+    state = { appTitle: "Customers" };
+
     render() {
-        return <div> Hello from Main content </div>
+        return (
+            <div>
+                <h4 className="border-bottom m-1 p-1"> {this.state.appTitle}</h4>
+            </div>
+        )
     }
 }
