45f52ce5faec39b897502302b2b67737bd7da3dd
mbugowski1 <mbugowski1@wp.pl>
2022-06-08 16:09:36 +0200

    Add common classes

---
+++ IceWallOw.Application/Common/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceWallOw.Application.Classes
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public ICollection<string>? Messages { get; set; }
+    }
+}
---
+++ IceWallOw.Application/Common/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceWallOw.Application.Classes
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Chat Chat { get; set; }
+    }
+}
