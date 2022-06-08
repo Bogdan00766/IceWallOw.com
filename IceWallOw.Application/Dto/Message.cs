ba9d99202908c5b6403c281df00e0edbd83efbab
mbugowski1 <mbugowski1@wp.pl>
2022-06-08 18:20:07 +0200

    Change internal to public in Message class

--- IceWallOw.Application/Dto/Message.cs
+++ IceWallOw.Application/Dto/Message.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IceWallOw.Application.Dto
 {
-    internal class Message
+    public class Message
     {
         public string Text { get; set; }
         public DateTime SendTime { get; set; }
