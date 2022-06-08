38f4e9d8e5c14d1fc3012474d0e3f61c7384ab33
Szymon1932 <szymon.kordal@student.wat.edu.pl>
2022-06-08 14:16:24 +0200

    obsluga1

--- IceWallOwWeb/Controllers/HomeController.cs
+++ IceWallOwWeb/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace IceWallOwWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
