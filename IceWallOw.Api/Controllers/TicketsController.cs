893ccf1d0df5df8be1c7166160f7253a2f326c61
mbugowski1 <mbugowski1@wp.pl>
2022-06-08 21:39:53 +0200

    Add reveiving tickets

--- IceWallOw.Api/Controllers/TicketsController.cs
+++ IceWallOw.Api/Controllers/TicketsController.cs
@@ -13,16 +13,15 @@ namespace IceWallOw.Api.Controllers
         private readonly ILogger<TicketsController> _logger;
         private readonly ITicketService _ticketService;
         private readonly IProducer<Null, int> _producer;
-        private static readonly ProducerConfig config = new ProducerConfig()
-        {
-            BootstrapServers = "kafka:29092"
-        };
+        private readonly IConsumer<Null, int> _consumer;
 
-        public TicketsController(ILogger<TicketsController> logger, ITicketService ticketService)
+        public TicketsController(ILogger<TicketsController> logger, IProducer<Null, int> producer,
+            IConsumer<Null, int> consumer, ITicketService ticketService)
         {
             _logger = logger;
             _ticketService = ticketService;
-            _producer = new ProducerBuilder<Null, int>(config).Build();
+            _producer = producer;
+            _consumer = consumer;
         }
         [HttpPost("CreateTicket")]
         public async Task<IActionResult> CreateTicket()
@@ -32,22 +31,34 @@ namespace IceWallOw.Api.Controllers
             _logger.LogInformation(0, "Creating token for " + user.Id);
             _logger.LogError(1, "Creating tokens not implemented");
 
-            throw new NotImplementedException("Pobieranie czatu nie zostalo zaimplementowane");
+            throw new NotImplementedException("Pobieranie ticketu nie zostalo zaimplementowane");
 
             var token = new TicketDto(0)
             {
-                Chat = new ChatDto(0)
+                Chat = new ChatDto(1)
             };
 
 
             _logger.LogInformation(2, $"Received tokenId {token.Id} for clientId {user.Id}");
             _logger.LogInformation(3, $"Sending tokenId {token.Id} to broker");
-            using(var producer = new ProducerBuilder<Null, int>(config).Build())
-            await _producer.ProduceAsync("Tokens", new Message<Null, int>()
+            await _producer.ProduceAsync("Tickets", new Message<Null, int>()
             {
                 Value = token.Id
             });
             _producer.Flush();
+            return Ok(token);
+        }
+
+        [HttpPost("GetNewTicket")]
+        public async Task<IActionResult> GetNewTicket()
+        {
+            _consumer.Subscribe("Tickets");
+            var message = await Task.Run(() => _consumer.Consume(TimeSpan.FromSeconds(10)));
+            if (message == null) return NoContent();
+            var token = new TicketDto(message.Message.Value)
+            {
+                Chat = new ChatDto(0)
+            };
             return Ok(token);
         }
     }
