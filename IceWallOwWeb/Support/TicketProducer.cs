bee5e5573918c4bb5df2b418dde5d6cf6c5ad3c5
mbugowski1 <mbugowski1@wp.pl>
2022-06-08 12:59:31 +0200

    Created producer for kafka

---
+++ IceWallOwWeb/Support/TicketProducer.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+
+namespace IceWallOwWeb.Support
+{
+    public class TicketProducer : IMessageProducer, IDisposable
+    {
+        private ILogger<TicketProducer> _logger;
+        private readonly IProducer<Null, int> _producer;
+
+        public TicketProducer(ILogger<TicketProducer> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "kafka:29092"
+            };
+            _producer = new ProducerBuilder<Null, int>(config).Build();
+        }
+        public async Task CreateMessage(int clientId, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Creating token for " + clientId);
+            _logger.LogError("Creating tokens not implemented");
+            throw new NotImplementedException("Creating tokens not implemented");
+            int tokenId = 1;
+            _logger.LogInformation($"Received tokenId {tokenId} for clientId {clientId}");
+            _logger.LogInformation($"Sending tokenId {tokenId} to broker");
+            await _producer.ProduceAsync("Tokens", new Message<Null, int>()
+            {
+                Value = clientId
+            }, cancellationToken);
+            _producer.Flush(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _producer.Dispose();
+        }
+    }
+}
