2e4ebf1fc5713ef9ffcf6a349b4ada68579e006e
Szymon1932 <szymon.kordal@student.wat.edu.pl>
2022-06-10 13:31:56 +0200

    unifying style of a header + linking to sites

--- IceWallOwWeb/Controllers/HomeController.cs
+++ IceWallOwWeb/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace IceWallOwWeb.Controllers
         {
             return View();
         }
+        public IActionResult Tickets()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
