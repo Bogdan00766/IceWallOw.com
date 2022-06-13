50bdc6070bc7006709b95627cbacdb46aa37a100
mbugowski1 <mbugowski1@wp.pl>
2022-06-13 13:03:24 +0200

    implemented IWebSocketService interface

--- IceWallOw.Application/Services/WebSocketService.cs
+++ IceWallOw.Application/Services/WebSocketService.cs
@@ -13,15 +13,29 @@ namespace IceWallOw.Application.Services
     public class WebSocketService : IChatService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IChatRepository _chatRepository;
+        private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
-        public WebSocketService(IUserRepository userRepository, IMapper mapper)
+        public WebSocketService(IUserRepository userRepository, IChatRepository chatRepository, IMessageRepository messageRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _chatRepository = chatRepository;
+            _messageRepository = messageRepository;
             _mapper = mapper;
         }
         public UserDto? FindUserByGuid(Guid guid)
         {
             return _mapper.Map<UserDto?>(_userRepository.FindUserByGUID(guid));
+        }
+
+        public Task<ICollection<MessageDto>> GetMessages(ChatDto chat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MessageDto> PutMessage(MessageDto message)
+        {
+            throw new NotImplementedException();
         }
     }
 }
